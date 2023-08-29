e72e1411f719a94065b21588eb6613bff80ee16a
joppe montezinos <joppe.montezinos@gmail.com>
2023-08-29 17:01:26 +0200

    test commit

--- app.vue
+++ app.vue
@@ -1,8 +1,20 @@
 <template>
   <NuxtLayout>
-    <ClientOnly>
-    </ClientOnly>
-    <model />
+    <NuxtPage/>
   </NuxtLayout>
 </template>
 
+<script setup>
+import { onMounted } from 'vue';
+import { trainModel } from './services/ai/tensorflow';
+
+onMounted(async () => {
+  console.log("Component Mounted"); 
+  try {
+    await trainModel();
+    console.log("Model trained");
+  } catch (error) {
+    console.error("Training failed", error);
+  }
+});
+</script>
