dc16c7da7449a42b9083aa0d80ddfa7314985163
joppe montezinos <joppe.montezinos@gmail.com>
2023-08-29 15:18:51 +0200

    test commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,21 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@pinia/nuxt"]
+  modules: ['@pinia/nuxt'],
+  build: {
+    extend(config, { isServer }) {
+      if (isServer) {
+        config.externals = {
+          fs: 'commonjs fs',
+        }
+      }
+    },
+    vite: {
+      optimizeDeps: {
+        exclude: ['fs']
+      },
+      rollupOptions: {
+        external: ['fs']
+      }
+    }
+  }
 })
--- services/ai/tensorflow.js
+++ services/ai/tensorflow.js
@@ -1,27 +1,43 @@
-import * as tf from '@tensorflow/tfjs-node';
-export const trainModel = async () => {
+let tf;
 
-    console.log("Train model started");
+if (process.server) {
+  tf = require('@tensorflow/tfjs-node');
+} else {
+  // Import the browser version or any other behavior you need on the client
+  tf = require('@tensorflow/tfjs');
+}
+export const trainModel = async () => {
+  console.log('Train model started');
   // Fetch JSON data from server
   const response = await fetch('http://localhost:3000/animeWithGenres.json');
   const rawData = await response.json();
-  console.log("Data fetched. Now training...");
-  const normalizedData = rawData.map(d => ({
+  console.log('Data fetched. Now training...');
+  const normalizedData = rawData.map((d) => ({
     ...d,
-    ageRating: d.ageRating / 10.0,
+    ageRating: d.ageRating / 10.0
   }));
 
-  const features = normalizedData.map(d => tf.tensor([...d.hotEncodedGenres, d.ageRating]));
+  const features = normalizedData.map((d) =>
+    tf.tensor([...d.hotEncodedGenres, d.ageRating])
+  );
 
   const model = tf.sequential();
 
-  model.add(tf.layers.dense({ units: 64, activation: 'relu', inputShape: [features[0].shape[0]] }));
+  model.add(
+    tf.layers.dense({
+      units: 64,
+      activation: 'relu',
+      inputShape: [features[0].shape[0]]
+    })
+  );
   model.add(tf.layers.dense({ units: 32, activation: 'relu' }));
-  model.add(tf.layers.dense({ units: features[0].shape[0], activation: 'sigmoid' }));
+  model.add(
+    tf.layers.dense({ units: features[0].shape[0], activation: 'sigmoid' })
+  );
 
   model.compile({
     optimizer: tf.train.adam(),
-    loss: 'meanSquaredError',
+    loss: 'meanSquaredError'
   });
 
   const xTrain = tf.stack(features);
@@ -29,7 +45,7 @@ export const trainModel = async () => {
 
   const history = await model.fit(xTrain, yTrain, {
     epochs: 50,
-    batchSize: 10,
+    batchSize: 10
   });
 
   console.log('Training complete', history);
@@ -38,10 +54,8 @@ export const trainModel = async () => {
   if (process.client) {
     await model.save('localstorage://anime-recommender');
   }
-  
 
   const predictions = model.predict(xTrain);
-
 };
 
 // Call the function and catch any errors
