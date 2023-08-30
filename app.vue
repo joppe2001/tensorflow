66142a1d83bf01248a0252d95f38b23714146ad0
joppe montezinos <joppe.montezinos@gmail.com>
2023-08-30 21:10:14 +0200

    made the new dataset

--- app.vue
+++ app.vue
@@ -18,7 +18,7 @@ const anime = ref('');
 const recommended = ref('');
 const loading = ref(false);
 
-const ignoreWords = ["of", "the", "and", "in", "on", "at", "or", "by", "no", "na"];
+const ignoreWords = ["of", "the", "and", "in", "on", "at", "or", "by", "na"];
 
 watch(anime, (newAnime) => {
   const words = newAnime.split(" ");
