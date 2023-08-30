7acc52bdda5bf33f2ad327f2b80a10366b0cf9f3
joppe montezinos <joppe.montezinos@gmail.com>
2023-08-30 14:22:56 +0200

    made the new dataset

--- app.vue
+++ app.vue
@@ -1,78 +1,69 @@
 <template>
-	<NuxtLayout>
-		<form action="submit" @submit.prevent="getRecommended">
-			<label for="anime">Anime</label>
-			<input type="text" id="anime" name="anime" v-model="anime" />
-			<button type="submit" :disabled="loading">Get Recommendation</button>
-			<p v-if="loading">Training model, please wait...</p>
-			<p v-if="recommended && !loading">Recommended: {{ recommended }}</p>
-		</form>
-	</NuxtLayout>
+  <NuxtLayout>
+    <form action="submit" @submit.prevent="getRecommended">
+      <label for="anime">Anime</label>
+      <input type="text" id="anime" name="anime" v-model="anime" />
+      <button type="submit" :disabled="loading">Get Recommendation</button>
+      <p v-if="loading">Training model, please wait...</p>
+      <p v-if="recommended && !loading">Recommended: {{ recommended }}</p>
+    </form>
+  </NuxtLayout>
 </template>
 
 <script setup>
-	import { onMounted, watch, ref } from "vue";
-	import { trainModel, getRecommendation } from "./services/tensorFlowOps.js";
+import { onMounted, watch, ref } from 'vue';
+import { trainModel, getRecommendation } from './services/tensorFlowOps.js'
 
-	const anime = ref("");
-	const recommended = ref("");
-	const loading = ref(true);
+const anime = ref('');
+const recommended = ref('');
+const loading = ref(true);
 
-	const ignoreWords = ["of", "the", "and", "in", "on", "at", "or", "by"];
+const ignoreWords = ["of", "the", "and", "in", "on", "at", "or", "by"];
 
-	watch(
-		anime,
-		(newAnime) => {
-			const words = newAnime.split(" ");
-			const capitalizedWords = words.map((word, index) => {
-				if (index === 0 || !ignoreWords.includes(word.toLowerCase())) {
-					return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-				} else {
-					return word.toLowerCase();
-				}
-			});
-			anime.value = capitalizedWords.join(" ");
-		},
-		{ immediate: true }
-	);
+watch(anime, (newAnime) => {
+  const words = newAnime.split(" ");
+  const capitalizedWords = words.map((word, index) => {
+    if (index === 0 || !ignoreWords.includes(word.toLowerCase())) {
+      return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    } else {
+      return word.toLowerCase();
+    }
+  });
+  anime.value = capitalizedWords.join(" ");
+}, { immediate: true });
 
-	onMounted(async () => {
-		console.log("Component Mounted");
-		try {
-			let response;
-			// Load anime data
-			if (process.client) {
-				response = await fetch("localstorage://anime-recommender");
-			} else if (process.server) {
-				response = await fetch("file://./public/anime-recommender");
-			}
-			const animeData = await response.json();
+onMounted(async () => {
+  console.log("Component Mounted");
+  try {
+    // Load anime data
+    const response = await fetch('/animeWithGenres.json');
+    const animeData = await response.json();
 
-			// Train the model
-			await trainModel(animeData);
-			console.log("Model trained");
-			loading.value = false;
-		} catch (error) {
-			console.error("Training failed", error);
-		}
-	});
+    // Train the model
+    await trainModel(animeData);
+    console.log("Model trained");
+    loading.value = false;
+  } catch (error) {
+    console.error("Training failed", error);
+  }
+});
 
-	const getRecommended = async () => {
-		if (loading.value) {
-			console.log("Model still training, cannot get recommendations yet.");
-			return;
-		}
-		try {
-			// Load anime data
-			const response = await fetch("/animeWithGenres.json");
-			const animeData = await response.json();
+const getRecommended = async () => {
+  if (loading.value) {
+    console.log("Model still training, cannot get recommendations yet.");
+    return;
+  }
+  try {
+    // Load anime data
+    const response = await fetch('/animeWithGenres.json');
+    const animeData = await response.json();
 
-			// Get a recommendation
-			const recommendation = await getRecommendation(animeData, anime.value);
-			console.log("Recommendation", recommendation);
-			recommended.value = recommendation;
-		} catch (error) {
-			console.error("Training failed", error);
-		}
-	};
+    // Get a recommendation
+    const recommendation = await getRecommendation(animeData, anime.value);
+    console.log("Recommendation", recommendation);
+    recommended.value = recommendation;
+  } catch (error) {
+    console.error("Training failed", error);
+  }
+};
 </script>
