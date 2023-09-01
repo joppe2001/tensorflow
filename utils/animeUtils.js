7a2abdf95836a54322cee59aa4ce27f5c306a847
joppe montezinos <joppe.montezinos@gmail.com>
2023-09-01 17:10:09 +0200

    adding data

--- utils/animeUtils.js
+++ utils/animeUtils.js
@@ -1,13 +1,12 @@
 export const findSimilarAnimes = (prediction, animeDataArray, animeName) => {
   // Set the weights for genre and age rating
-  const genreWeight = 1; // Feel free to change
-  const ageWeight = 1; // Feel free to change,
+  const genreWeight = 3; // Feel free to change
+  const ageWeight = 2; // Feel free to change,
   const scoreWeight = 1; // Feel free to change
-  const themeWeight = 10; // Feel free to change
+  const themeWeight = 8; // Feel free to change
   const demographicWeight = 5; // Feel free to change
 
   const dists = animeDataArray.map((anime) => {
-    // Apply weights to the features
     const features = [
       ...anime.hotEncodedGenres.map((genre) => genre * genreWeight),
       (anime.ageRating / 10.0) * ageWeight,
