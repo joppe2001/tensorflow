7867792aa45e31b35ff578daabdeadfa78387862
joppe montezinos <joppe.montezinos@gmail.com>
2023-09-04 22:55:08 +0200

    adding softmax instead of sigmoid

--- services/tensorFlowOps.js
+++ services/tensorFlowOps.js
@@ -1,12 +1,11 @@
 import * as tf from '@tensorflow/tfjs';
 import { findSimilarAnimes } from '#imports';
+import { extractDuration, ratingToInt, oneHotEncodeGenre } from '../utils/animeUtils'
 
-export const getRecommendation = async (animeData, animeName) => {
+
+export const getRecommendation = async (animeData, animeName, genreList) => {
   const loadedModel = await tf.loadLayersModel('/anime-recommender/model.json');
-  console.log(
-    'Model loaded. Input shape expected:',
-    loadedModel.inputs[0].shape
-  );
+  console.log('Model loaded. Input shape expected:', loadedModel.inputs[0].shape);
 
   const chosenAnime = animeData.find((anime) => anime.title === animeName);
   if (!chosenAnime) {
@@ -15,23 +14,19 @@ export const getRecommendation = async (animeData, animeName) => {
   }
 
   const chosenFeatures = tf.tensor([
-    ...chosenAnime.hotEncodedGenres,
-    chosenAnime.ageRating,
+    chosenAnime.episodes * extractDuration(chosenAnime.duration),
+    chosenAnime.status === "Finished Airing" ? 1 : 0,
+    chosenAnime.airing === "True" ? 1 : 0,
+    ratingToInt(chosenAnime.rating),
     chosenAnime.score,
-    chosenAnime.normalizedEpisodes,
-    chosenAnime.normalizedRank,
-    chosenAnime.normalizedPopularity,
-    ...chosenAnime.hotEncodedThemes,
-    ...chosenAnime.hotEncodedDemographics
+    chosenAnime.rank,
+    chosenAnime.popularity,
+    ...oneHotEncodeGenre(chosenAnime.genre, genreList)
   ]);
 
   const prediction = loadedModel.predict(chosenFeatures.reshape([1, -1]));
   const predictionArray = await prediction.array();
-  const similarAnimes = findSimilarAnimes(
-    predictionArray[0],
-    animeData,
-    animeName
-  );
+  const similarAnimes = findSimilarAnimes(predictionArray[0], animeData, animeName);
 
   console.log('Recommended Animes:', similarAnimes);
   return similarAnimes;
--- services/test.mjs
+++ services/test.mjs
@@ -1,73 +1,152 @@
-import * as tf from '@tensorflow/tfjs-node';
-import fs from 'fs/promises';
+import * as tf from "@tensorflow/tfjs-node";
+import fs from "fs/promises";
+import {
+	extractDuration,
+	ratingToInt,
+	oneHotEncodeGenre,
+} from "../utils/animeUtils";
 
-const normalizeTensor = (tensor) => {
-  const min = tensor.min();
-  const max = tensor.max();
-  return tensor.sub(min).div(max.sub(min));
+// Helper functions
+const genreList = [
+	"Action",
+	"Adventure",
+	"Avant Garde",
+	"Award Winning",
+	"Boys Love",
+	"Comedy",
+	"Drama",
+	"Fantasy",
+	"Girls Love",
+	"Gourmet",
+	"Horror",
+	"Mystery",
+	"Romance",
+	"Sci-Fi",
+	"Slice of Life",
+	"Sports",
+	"Supernatural",
+	"Suspense",
+	"ecchi",
+	"erotica",
+	"hentai",
+]; // Include all possible genres
+
+const oneHotEncodeGenre = (genreStr) =>
+	genreList.map((genre) => (genreStr.includes(genre) ? 1 : 0));
+
+const splitData = (data, trainSize, valSize) => {
+	const shuffled = tf.util.shuffle(data);
+	const trainEnd = Math.floor(trainSize * shuffled.length);
+	const valEnd = trainEnd + Math.floor(valSize * shuffled.length);
+	return {
+		train: shuffled.slice(0, trainEnd),
+		val: shuffled.slice(trainEnd, valEnd),
+		test: shuffled.slice(valEnd),
+	};
+};
+
+const scoreToClass = (score) => {
+	if (score < 5) {
+		return 0;
+	} else if (score < 6) {
+		return 1;
+	} else if (score < 7) {
+		return 2;
+	} else if (score < 8) {
+		return 3;
+	} else if (score < 9) {
+		return 4;
+	} else {
+		return 5;
+	}
+};
+
+const oneHotEncodeScore = (score) => {
+	const classes = 6;
+	const index = scoreToClass(score);
+	return Array(classes)
+		.fill(0)
+		.map((_, i) => (i === index ? 1 : 0));
 };
 
 const trainModel = async (animeData) => {
-  console.log("Train model started");
+	console.log("Train model started");
 
-  const features = animeData.map(
-    (d) => tf.tensor([
-      ...d.hotEncodedGenres, 
-      d.ageRating, 
-      d.score, 
-      d.normalizedEpisodes, 
-      d.normalizedRank, 
-      d.normalizedPopularity,
-      ...d.hotEncodedThemes,
-      ...d.hotEncodedDemographics,
-    ])
-  );
+	const tensors = animeData.map((d) => ({
+		features: tf.tensor([
+			d.episodes * extractDuration(d.duration),
+			d.status === "Finished Airing" ? 1 : 0,
+			d.airing === "True" ? 1 : 0,
+			ratingToInt(d.rating),
+			d.rank,
+			d.popularity,
+			...oneHotEncodeGenre(d.genre),
+		]),
+		label: tf.tensor(oneHotEncodeScore(d.score)),
+	}));
 
-  const model = tf.sequential();
+	// Splitting data into training, validation, and test sets
+	const splitSizes = { train: 0.7, val: 0.15 };
+	const { train, val, test } = splitData(
+		tensors,
+		splitSizes.train,
+		splitSizes.val
+	);
 
-  model.add(
-    tf.layers.dense({
-      units: 64,
-      activation: "relu",
-      inputShape: [features[0].shape[0]],
-    })
-  );
-  model.add(tf.layers.dense({ units: 32, activation: "relu" }));
-  model.add(
-    tf.layers.dense({ units: features[0].shape[0], activation: "sigmoid" })
-  );
+	const model = tf.sequential();
 
-  model.compile({
-    optimizer: tf.train.adam(),
-    loss: "meanSquaredError",
-  });
+	model.add(
+		tf.layers.dense({
+			units: 64,
+			activation: "relu",
+			inputShape: [train[0].features.shape[0]],
+		})
+	);
 
-  const xTrain = tf.stack(features);
-  const yTrain = tf.stack(features);
+	model.add(tf.layers.dense({ units: 32, activation: "relu" }));
 
-  const history = await model.fit(xTrain, yTrain, {
-    epochs: 50,
-    batchSize: 100,
-  });
+	// Change this part for classification
+	model.add(
+		tf.layers.dense({
+			units: 6, // The number of classes you have
+			activation: "softmax",
+		})
+	);
 
-  console.log("Training complete");
-  console.log(history)
-  console.log("Epochs:", history.epoch[history.epoch.length - 1]);
-  console.log(
-    "Last Loss Value:",
-    history.history.loss[history.epoch.length - 1]
-  );
+	model.compile({
+		optimizer: tf.train.adam(),
+		loss: "categoricalCrossentropy",
+		metrics: ["accuracy"],
+	});
 
-  await model.save("file://../public/anime-recommender");
-  console.log("Model saved to ./public/anime-recommender.");
+	const xTrain = tf.stack(train.map((item) => item.features));
+	const yTrain = tf.stack(train.map((item) => item.label));
+	const xVal = tf.stack(val.map((item) => item.features));
+	const yVal = tf.stack(val.map((item) => item.label));
+
+	const history = await model.fit(xTrain, yTrain, {
+		epochs: 50,
+		batchSize: 100,
+		validationData: [xVal, yVal],
+	});
+
+	console.log("Training complete");
+	console.log("Epochs:", history.epoch[history.epoch.length - 1]);
+	console.log(
+		"Last Loss Value:",
+		history.history.loss[history.epoch.length - 1]
+	);
+
+	await model.save("file://./public/anime-recommender");
+	console.log("Model saved to ./public/anime-recommender.");
 };
 
 const readFile = async () => {
-  const data = await fs.readFile('../public/hotEncodedAnime.json', 'utf-8');
-  return JSON.parse(data);
+	const data = await fs.readFile("../public/csvjson.json", "utf-8");
+	return JSON.parse(data);
 };
 
 (async () => {
-  const animeData = await readFile();
-  await trainModel(animeData);
+	const animeData = await readFile();
+	await trainModel(animeData);
 })();
