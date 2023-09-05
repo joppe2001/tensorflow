f0b75ffb3eac360ca0c90e5c616e5a8c42ff660d
joppe montezinos <joppe.montezinos@gmail.com>
2023-09-05 02:43:35 +0200

    test

--- services/filteredJsonScript.mjs
+++ services/filteredJsonScript.mjs
@@ -3,8 +3,25 @@ const fs = require('fs');
 const rawData = fs.readFileSync("../public/csvjson.json", "utf8");
 const animeList = JSON.parse(rawData);
 
-const filteredAnimeList = animeList.filter(
-  (anime) => anime.episodes !== 0 && anime.genre !== "" && anime.duration !== "Uknown"
-);
+const filteredAnimeList = animeList.filter(d => {
+    const episodes = d.episodes || 0;
+    const duration = d.duration || 'Unknown';
+    const status = d.status || '';
+    const airing = d.airing || '';
+    const rating = d.rating || '';
+    const rank = d.rank || 0;
+    const popularity = d.popularity || 0;
+    const genre = d.genre || ''; 
 
-fs.writeFileSync("./filteredJson.json", JSON.stringify(filteredAnimeList, null, 2));
+    // Check if the values are not in the default state
+    return episodes !== 0 && 
+           duration !== 'Unknown' && 
+           status !== '' && 
+           airing !== '' && 
+           rating !== '' && 
+           rank !== 0 && 
+           popularity !== 0 && 
+           genre !== '';
+});
+
+fs.writeFileSync("./filteredJson2.json", JSON.stringify(filteredAnimeList, null, 2));
